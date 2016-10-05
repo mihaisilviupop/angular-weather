31a07f83c1f4e277d27b913414c17e25cd5ce9f5
Mihai Pop <Mihai Pop>
2016-10-05 23:23:06 +0300

    add bootstrap, bootstrap-ui

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'ui.bootstrap'])
 
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/view1', {
@@ -9,28 +9,48 @@ angular.module('myApp.view1', ['ngRoute'])
     });
   }])
 
-  .controller('View1Ctrl', ['$scope', '$http', function ($scope, $http) {
+  .factory("Countries", function ($http) {
+    return $http.get('countries.json');
+  })
+
+  .controller('View1Ctrl', ['$scope', '$http', 'Countries', function ($scope, $http, Countries) {
     var apiId = 'c170242baa576ba9cab6a1926c40e3fb';
-    var url = 'http://api.openweathermap.org/data/2.5/weather?';
+    var url = 'http://api.openweathermap.org/data/2.5/';
     var units = "metric";
     var lang = "en";
     var latLng = {};
+    $scope.selectedCountry = undefined;
+
+    Countries.success(function (data) {
+      $scope.countries = data;
+      console.log($scope.countries, $scope.states);
+    });
+
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function (position) {
         latLng = {
           'lat': position.coords.latitude,
           'lng': position.coords.longitude
         };
-        url += "lat=" + latLng.lat + "&lon=" + latLng.lng + "&units=" + units + "&lang=" + lang + "&appid=" + apiId;
+        url += "weather?" + "lat=" + latLng.lat + "&lon=" + latLng.lng + "&units=" + units + "&lang=" + lang + "&appid=" + apiId;
         $http.get(url).then(function (response) {
           $scope.weather = response.data;
           console.log($scope.weather);
         });
-      },
-        function (err) {
-          console.log("err", err);
-        });
+      }, function (err) {
+        console.error("error", err);
+      })
     } else {
       console.log("Geolocation is not supported by this browser.");
     }
+
+    $scope.getWeatherForCountry = function (country) {
+      //http://api.openweathermap.org/data/2.5/find?q=Romania&type=like&appid=c170242baa576ba9cab6a1926c40e3fb 
+      url += "find?q=" + country + "&type=like" + "&units=" + units + "&lang=" + lang + "&appid=" + apiId;
+      $http.get(url).then(function (response) {
+        $scope.weather = response.data.list[0];
+        console.log($scope.weather);
+      });
+    }
+
   }]);
